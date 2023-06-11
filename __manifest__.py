4c00a5a7088f1581e10f6838f37513837acce8fe
Steven Giron <maxpowersga@gmail.com>
2023-06-10 19:34:02 -0500

    website.menu added

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,19 @@
 {
     "name": "Universidad UNIACME",
-    "application": True,
-    "installable": True,
+    "author": "Steven Giron Arcila",
     "depends": [
         "base",
         "base_geolocalize",
+        "website",
 
     ],
     "data": [
+        "security/ir.model.access.csv",
         "views/campus.views.xml",
         "views/student.views.xml",
         "views/voting_process_views.xml",
+        "views/voting_website_form.xml",
     ],
+    "application": True,
+    "installable": True,
 }
